e9fda777edc50f2f92698f6a48d7d3cf058a768f
shahidul islam <shahidulislamshohid7@gmail.com>
2026-08-16 08:34:10 +0600

    Initial commit

---
+++ frontend/src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-xl font-bold text-white-600">Hello World</Text>
+    </View>
+  );
+}
