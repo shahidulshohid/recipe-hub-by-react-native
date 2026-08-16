6651cde52d08cbb750f1aad01723f2492401add8
shahidul islam <shahidulislamshohid7@gmail.com>
2026-08-16 09:25:44 +0600

    feat: initialize project structure and setup Expo frontend with basic layout

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-xl font-bold text-white-600">Hello World</Text>
+    <View className="flex-1 justify-center items-center bg-gray-100">
+      <Text className="text-2xl font-bold text-blue-600">Hello Worldsfdsfd updated!</Text>
     </View>
   );
 }
